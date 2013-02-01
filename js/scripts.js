70548b31aa46a5d2d79cfb366cc83dff4a67bbf1
James McKinney <james@slashpoundbang.com>
2013-02-01 17:59:23 -0500

    fix tabs

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ $('.span9 .tab-content div').each(function () {
   var $this = $(this);
   $.get($this.data('url'), function (data) {
     $this.html('<pre>' + $('<div/>').text(data).html() + '</pre>');
-  });
+  }, 'html');
 });
 $('.span9 .nav-tabs a').click(function (e) {
   e.preventDefault();
