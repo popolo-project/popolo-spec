f7706d8fc70fc7135f6882c8dcebcdf009793eff
James McKinney <james@slashpoundbang.com>
2013-06-21 18:46:27 -0400

    add download link for embedded json, ttl files

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ $('.tab-content.no-js div').each(function () {
   var $this = $(this);
   if ($this.data('url')) {
     $.get($this.data('url'), function (data) {
-      $this.html('<pre>' + $('<div/>').text(data).html() + '</pre>');
+      $this.html('<a class="download-link" href="' + $this.data('url') + '">Download</a><pre>' + $('<div/>').text(data).html() + '</pre>');
     }, 'html');
   }
 });
